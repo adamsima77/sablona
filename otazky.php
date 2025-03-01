aef300a4d7dd9d34ac79ee6e50ed07bf546b9c4d
Adam Šima <adamsima77@gmail.com>
2025-03-01 18:45:17 +0100

    Dynamický akordeón

---
+++ otazky.php
@@ -0,0 +1,47 @@
+<?php
+$otazky = array(
+
+  
+ "Aký je rozdiel medzi jazykom Python a jazykom C++?",
+ "Čo je to objektovo orientované programovanie (OOP)?",
+ "Čo znamená výraz kompilácia v kontexte programovania?",
+ "Aký je rozdiel medzi jazykmi JavaScript a TypeScript?",
+ "Čo je to SQL a na čo sa používa?"
+
+
+
+); 
+
+
+
+$odpovede = array(
+
+  
+"Python je interpretovaný jazyk, ktorý je známy svojou jednoduchosťou a čitateľnosťou. Používa sa často na skriptovanie, analýzu dát a vývoj webových aplikácií. Na druhej strane, C++ je kompilovaný jazyk, ktorý je rýchlejší a efektívnejší, ale vyžaduje zložitejší kód. 
+C++ sa často používa na vývoj systémového softvéru a aplikácií, kde je potrebná vysoká výkonnosť.",
+
+"Objektovo orientované programovanie (OOP) je paradigmou programovania, ktorá sa zameriava na objekty a ich vzájomné interakcie. V OOP sa kód organizuje do tried (objektov), ktoré obsahujú vlastnosti (atribúty) a metódy (funkcie).
+Tento prístup uľahčuje opätovné použitie kódu a jeho údržbu.",
+
+"Kompilácia je proces, pri ktorom sa zdrojový kód napísaný v programovacom jazyku (napr. C++) prevedie do spustiteľného súboru, ktorý môže počítač vykonať. 
+Tento proces zvyčajne zahŕňa preklad kódu do strojového jazyka, ktorý je špecifický pre danú platformu.",
+
+"JavaScript je interpretovaný jazyk používaný na tvorbu interaktívnych webových stránok. TypeScript je nadmnožinou JavaScriptu, ktorá pridáva podporu pre statickú typizáciu. TypeScript umožňuje vývojárom deklarovať typy pre premenné, funkcie a objekty, čo zvyšuje 
+bezpečnosť a čitateľnosť kódu. TypeScript sa následne prekladá do JavaScriptu.",
+
+"SQL (Structured Query Language) je jazyk určený na správu
+ a manipuláciu s databázami. Používa sa na vykonávanie operácií ako je získavanie, vkladanie, 
+ aktualizovanie alebo mazanie dát v relačných databázach. SQL je nevyhnutný pre prácu s databázovými systémami 
+ ako MySQL, PostgreSQL alebo SQLite.",
+
+
+
+); 
+
+
+
+
+
+
+
+?>
