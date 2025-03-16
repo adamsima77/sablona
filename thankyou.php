354ab6d7c8f79c9455ab670a9c03080e3d9f2818
Adam Šima <adamsima77@gmail.com>
2025-03-16 09:06:28 +0100

    Úprava

---
+++ thankyou.php
@@ -0,0 +1,20 @@
+<?php include_once "parts/header.php"?>
+<?php include_once "parts/nav.php" ?>
+    <main>
+      <section class="banner">
+        <div class="container text-white">
+          <h1>Ďakujeme</h1>
+        </div>
+      </section>
+      <section class="container">
+        <div class="row">
+          <div class="col-100 text-center">
+              <h2>Ďakujeme za vyplnenie formulára</h2>
+          </div>
+        </div>
+      </section>
+
+
+    </main>
+    
+ <?php include_once "parts/footer.php"?>
